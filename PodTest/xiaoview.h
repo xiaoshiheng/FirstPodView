aa9556ea8b5f9ae4ca1f7000f27bc4614c360a7c
Mrxiao <xiaoshiheng@xiaoshihengdeMac-mini.local>
2016-08-24 14:56:16 +0800

    asdjflkf

---
+++ PodTest/xiaoview.h
@@ -0,0 +1,13 @@
+//
+//  xiaoview.h
+//  testtest
+//
+//  Created by Mrxiao on 16/6/15.
+//  Copyright © 2016年 MRxiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface xiaoview : UIView
+
+@end
---
+++ PodTest/xiaoview.m
@@ -0,0 +1,21 @@
+//
+//  xiaoview.m
+//  testtest
+//
+//  Created by Mrxiao on 16/6/15.
+//  Copyright © 2016年 MRxiao. All rights reserved.
+//
+
+#import "xiaoview.h"
+
+@implementation xiaoview
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
